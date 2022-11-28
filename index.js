c60e4a2e7572107dd417617539ff163a9c5807bb
Giovana Rocha <100931504+giovanarocha09@users.noreply.github.com>
2022-11-28 19:47:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+//Armazenar os numeros que o usuario escolher
 
+const numerosapostados = []
+
+const aposta =document.getElementById('aposta');
+aposta.disabled = true;
+
+
+function selecionarnumeros(numero){
+
+    if(numerosapostados.length >= 0 && numerosapostados.length < 15){
+
+         //adiciona o numero na lista
+        numerosapostados.push(numero)
+
+        //desabilita o numeros que o usuario escolher
+        desabilitarnumeroescolhido(numero);
+    }
+}
+
+function desabilitarnumeroescolhido(numero){
+    document.getElementById('num_' + numero).disabled = true;
+    document.getElementById('num_' + numero).style.background = "#1ca445";
+    document.getElementById('num_' + numero).style.color = "#fff";
+    
+}
 
 
 
