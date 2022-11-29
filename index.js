348692981152b97a9e0bc01380a2efbff7349ef5
Giovana Rocha <100931504+giovanarocha09@users.noreply.github.com>
2022-11-28 23:12:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ function selecionarnumeros(numero){
 
     if(numerosapostados.length >= 0 && numerosapostados.length < 15){
 
-         //adiciona o numero na lista
+        //adiciona o numero na lista
         numerosapostados.push(numero)
 
         //desabilita o numeros que o usuario escolher
         desabilitarnumeroescolhido(numero);
+
+        //Habilitar o botão apostar
+        if (numerosapostados.length > 5){
+            aposta.disabled = false;
+        }
+
+        //Mostrar quant de num apostados 
+        const qtdaposta = document.getElementById("quant");
+        qtdaposta.innerHTML = "<p>Quant numeros</p><p class='destac'>" + numerosapostados.length + "</p>";
+        
     }
+
+
 }
 
 function desabilitarnumeroescolhido(numero){
