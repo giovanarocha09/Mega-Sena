42209c992a2178ac26e4f384cbb3dbbf6ae669ca
Giovana Rocha <100931504+giovanarocha09@users.noreply.github.com>
2022-11-28 23:30:24 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-//Armazenar os numeros que o usuario escolher
+//Variáveis 
 
 const numerosapostados = []
 
 const aposta =document.getElementById('aposta');
 aposta.disabled = true;
+
+let valoraposta = 0
+
+
 
 
 function selecionarnumeros(numero){
@@ -19,6 +23,9 @@ function selecionarnumeros(numero){
         //Habilitar o botão apostar
         if (numerosapostados.length > 5){
             aposta.disabled = false;
+
+            //Mostrar o valor da aposta
+            valordaaposta()
         }
 
         //Mostrar quant de num apostados 
@@ -30,6 +37,7 @@ function selecionarnumeros(numero){
 
 }
 
+//Funcao para o numeros que o usuario escolher
 function desabilitarnumeroescolhido(numero){
     document.getElementById('num_' + numero).disabled = true;
     document.getElementById('num_' + numero).style.background = "#1ca445";
@@ -37,5 +45,57 @@ function desabilitarnumeroescolhido(numero){
     
 }
 
+// Funcao para Mostrar o valor da aposta
+
+function valordaaposta(){
+    switch(numerosapostados.length){
+
+        case 6: 
+            valoraposta =  "R$ 4,50 "
+            break;
+
+        case 7: 
+            valoraposta =  "R$ 31,50 "
+            break;
+
+        case 8: 
+            valoraposta =  "R$ 126,00 "
+            break;
+
+        case 9: 
+            valoraposta =  "R$ 378,00 "
+            break;
+
+        case 10: 
+            valoraposta =  "R$ 945,00 "
+            break;
+
+        case 11: 
+            valoraposta =  "R$ 2.079,00"
+            break;
+
+        case 12: 
+            valoraposta =  "R$ 4.158,00 "
+            break;
+
+        case 13: 
+            valoraposta =  "R$ 6.006,00 "
+            break;
+
+        case 14: 
+            valoraposta =  "R$ 10.510,50 "
+            break;
+
+        case 15: 
+            valoraposta =  "R$ 17.517,50 "
+            break;
+
+        default:
+            valoraposta =  "R$ 00,00"
+            break;
 
 
+    }
+    const divvaloraposta = document.getElementById("valor");
+    divvaloraposta.innerHTML = "<p>Valor da Aposta</p> <p class='destac'>" + valoraposta + "</p>"
+}
